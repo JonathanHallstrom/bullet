ef7c2a2a19516a697a78e1039b363ed59ca019ee
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-01-22 12:33:47 +0100

    int multilayer

---
+++ examples/pawnocchio_multilayer.rs
@@ -0,0 +1,251 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use bullet_lib::{
+    game::{inputs::ChessBucketsMirrored, outputs::MaterialCount},
+    nn::{
+        InitSettings, Shape,
+        optimiser::{AdamW, AdamWParams},
+    },
+    trainer::{
+        save::SavedFormat,
+        schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
+        settings::LocalSettings,
+    },
+    value::{
+        ValueTrainerBuilder,
+        loader::{ViriBinpackLoader, viribinpack::ViriFilter},
+    },
+};
+use bytemuck::zeroed;
+use rand::{Rng, rng};
+use viriformat::{
+    chess::{board::Board, chessmove::Move},
+    dataformat::{Filter, WDL},
+};
+
+type Optimiser = AdamW;
+type OptimiserParams = AdamWParams;
+const NET_NAME: &'static str = "pawnocchio_multilayer_2048";
+
+const SUPERBATCHES_STAGE1: usize = 800;
+const SUPERBATCHES_STAGE2: usize = 200;
+const L1: usize = 2048;
+const L2: usize = 16;
+const L3: usize = 32;
+const SCALE: i32 = 400;
+const Q0: i16 = 255;
+const Q1: i16 = 128;
+const Q: i16 = 64;
+const INPUT_BUCKETS: usize = 16;
+const OUTPUT_BUCKETS: usize = 8;
+
+const FT_SHIFT: usize = 8;
+const FT_SHIFT_SCALE: f32 = Q0 as f32 / ((1 << FT_SHIFT) as f32);
+const I8_RANGE: f32 = i8::MAX as f32 / (Q1 as f32);
+const L1_RANGE: f32 = I8_RANGE * FT_SHIFT_SCALE * FT_SHIFT_SCALE;
+
+#[rustfmt::skip]
+const BUCKET_LAYOUT: [usize; 32] = [
+     0,  1,  2,  3,
+     4,  5,  6,  7,
+     8,  8,  9,  9,
+    10, 10, 11, 11,
+    12, 12, 13, 13,
+    12, 12, 13, 13,
+    14, 14, 15, 15,
+    14, 14, 15, 15,
+];
+
+fn piece_count_acceptance(board: &Board) -> f64 {
+    #[rustfmt::skip]
+    const DESIRED_DISTRIBUTION: [f64; 33] = [
+        0.018411966423, 0.020641545085, 0.022727271053,
+        0.024669162740, 0.026467201733, 0.028121406444,
+        0.029631758462, 0.030998276198, 0.032220941240,
+        0.033299772000, 0.034234750067, 0.035025893853,
+        0.035673184944, 0.036176641754, 0.036536245870,
+        0.036752015705, 0.036823932846, 0.036752015705,
+        0.036536245870, 0.036176641754, 0.035673184944,
+        0.035025893853, 0.034234750067, 0.033299772000,
+        0.032220941240, 0.030998276198, 0.029631758462,
+        0.028121406444, 0.026467201733, 0.024669162740,
+        0.022727271053, 0.020641545085, 0.018411966423,
+    ];
+
+    static PIECE_COUNT_STATS: [AtomicU64; 33] = zeroed();
+    static PIECE_COUNT_TOTAL: AtomicU64 = AtomicU64::new(0);
+
+    let pc = board.pieces.occupied().count() as usize;
+    let count = PIECE_COUNT_STATS[pc].fetch_add(1, Ordering::Relaxed) + 1;
+    let total = PIECE_COUNT_TOTAL.fetch_add(1, Ordering::Relaxed) + 1;
+    let frequency = count as f64 / total as f64;
+
+    // Calculate the acceptance probability for this piece count
+    let acceptance = 0.5 * DESIRED_DISTRIBUTION[pc] / frequency;
+    acceptance.clamp(0., 1.)
+}
+fn filter(board: &Board, mv: Move, eval: i16, wdl: f32) -> bool {
+    let default_viri_filter = Filter {
+        min_ply: 16,
+        min_pieces: 4,
+        filter_tactical: true,
+        filter_check: true,
+        filter_castling: true,
+        max_eval: 10000,
+        max_eval_incorrectness: 2500,
+        random_fen_skipping: true,
+        random_fen_skip_probability: 0.15,
+
+        wdl_filtered: false,
+        wdl_model_params_a: [-51.91819866, 145.18809272, -166.61481017, 281.59570002],
+        wdl_model_params_b: [-24.71724508, 82.92975519, -33.49186286, 52.86407201],
+        ..Default::default()
+    };
+    let mut rng = rng();
+    let wdl = match wdl {
+        1.0 => WDL::Win,
+        0.5 => WDL::Draw,
+        0.0 => WDL::Loss,
+        _ => unreachable!(),
+    };
+
+    !default_viri_filter.should_filter(mv, eval as i32, board, wdl, &mut rng)
+        && rng.random_bool(piece_count_acceptance(board))
+}
+fn main() {
+    let mut trainer = ValueTrainerBuilder::default()
+        .dual_perspective()
+        .optimiser(Optimiser::default())
+        .inputs(ChessBucketsMirrored::new(BUCKET_LAYOUT))
+        .output_buckets(MaterialCount::<OUTPUT_BUCKETS>)
+        .save_format(&[
+            SavedFormat::id("l0w")
+                .transform(|builder, mut weights| {
+                    let factoriser = builder.get("l0f").values;
+                    let expanded = factoriser.repeat(INPUT_BUCKETS);
+
+                    for (i, &j) in weights.iter_mut().zip(expanded.iter()) {
+                        *i += j;
+                    }
+
+                    weights
+                })
+                .round()
+                .quantise::<i16>(Q0),
+            SavedFormat::id("l0b").round().quantise::<i16>(Q0),
+            SavedFormat::id("l1w").round().quantise::<i8>(Q1),
+            SavedFormat::id("l1b").round().quantise::<i32>(Q1 as i32),
+            SavedFormat::id("l2w").round().quantise::<i32>(Q as i32),
+            SavedFormat::id("l2b").round().quantise::<i32>((Q as i32).pow(3)),
+            SavedFormat::id("l3w").round().quantise::<i32>(Q as i32),
+            SavedFormat::id("l3b").round().quantise::<i32>((Q as i32).pow(4)),
+        ])
+        .loss_fn(|output, target| output.sigmoid().squared_error(target))
+        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
+            // input layer factoriser
+            let l0f = builder.new_weights("l0f", Shape::new(L1, 768), InitSettings::Zeroed);
+            let expanded_factoriser = l0f.repeat(INPUT_BUCKETS);
+
+            // input layer weights
+            let mut l0 = builder.new_affine("l0", 768 * INPUT_BUCKETS, L1);
+            l0.weights = l0.weights + expanded_factoriser;
+
+            // output layer weights
+            let l1 = builder.new_affine("l1", L1, OUTPUT_BUCKETS * L2);
+            let l2 = builder.new_affine("l2", L2, OUTPUT_BUCKETS * L3);
+            let l3 = builder.new_affine("l3", L3, OUTPUT_BUCKETS);
+
+            // inference
+            let stm_hidden = l0.forward(stm_inputs).crelu().pairwise_mul();
+            let ntm_hidden = l0.forward(ntm_inputs).crelu().pairwise_mul();
+            let hl1 = stm_hidden.concat(ntm_hidden);
+            let hl2 = l1.forward(hl1).select(output_buckets).screlu();
+            let hl3 = l2.forward(hl2).select(output_buckets).crelu();
+            l3.forward(hl3).select(output_buckets)
+        });
+    let l0_clip = OptimiserParams { max_weight: 0.99, min_weight: -0.99, ..Default::default() };
+    trainer.optimiser.set_params_for_weight("l0w", l0_clip);
+    trainer.optimiser.set_params_for_weight("l0f", l0_clip);
+
+    let l1_clip = OptimiserParams { max_weight: L1_RANGE, min_weight: -L1_RANGE, ..Default::default() };
+    trainer.optimiser.set_params_for_weight("l1w", l1_clip);
+
+    let stage1_schedule = TrainingSchedule {
+        net_id: NET_NAME.to_string() + "_stage1",
+        eval_scale: SCALE as f32,
+        steps: TrainingSteps {
+            batch_size: 16_384 * 4,
+            batches_per_superbatch: 6104 / 4,
+            start_superbatch: 1,
+            end_superbatch: SUPERBATCHES_STAGE1,
+        },
+        wdl_scheduler: wdl::LinearWDL { start: 0.25, end: 0.85 },
+        lr_scheduler: lr::Warmup {
+            inner: lr::LinearDecayLR {
+                initial_lr: 0.001,
+                final_lr: 0.001 * f32::powi(0.3, 5),
+                final_superbatch: SUPERBATCHES_STAGE1,
+            },
+            warmup_batches: 200,
+        },
+        save_rate: 100,
+    };
+    let stage2_schedule = TrainingSchedule {
+        net_id: NET_NAME.to_string() + "_stage2",
+        eval_scale: SCALE as f32,
+        steps: TrainingSteps {
+            batch_size: 16_384 * 4,
+            batches_per_superbatch: 6104 / 4,
+            start_superbatch: 1,
+            end_superbatch: SUPERBATCHES_STAGE2,
+        },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.85 },
+        lr_scheduler: lr::Warmup {
+            inner: lr::ExponentialDecayLR {
+                initial_lr: 0.001 * f32::powi(0.3, 5),
+                final_lr: 0.001 * f32::powi(0.3, 7),
+                final_superbatch: SUPERBATCHES_STAGE2,
+            },
+            warmup_batches: 200,
+        },
+        save_rate: 100,
+    };
+
+    let settings =
+        LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 1024 };
+
+    let binpack_dataset =
+        "/media/jonathanhallstrom/64a18cc9-6680-4f1b-a09f-56b812251151/vine_data/vine_37/mixed_data.vf";
+
+    // trainer.run(
+    //     &stage1_schedule,
+    //     &settings,
+    //     &ViriBinpackLoader::new(binpack_dataset, 8192, 16, ViriFilter::Custom(filter)),
+    // );
+    // trainer.run(
+    //     &stage2_schedule,
+    //     &settings,
+    //     &ViriBinpackLoader::new(binpack_dataset, 8192, 16, ViriFilter::Custom(filter)),
+    // );
+
+    trainer.load_from_checkpoint("checkpoints/pawnocchio_multilayer_2048_stage2-200");
+    trainer.save_to_checkpoint("checkpoints/pawnocchio_multilayer_2048_stage2-200_requantise");
+
+    for fen in [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/P2P2PP/q2Q1R1K w kq - 0 2",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR w KQkq - 0 1",
+        "rnb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "rn1qkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r1bqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "1nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQka - 0 1",
+    ] {
+        let eval = trainer.eval(fen);
+        println!("FEN: {fen}");
+        println!("EVAL: {}", 400.0 * eval);
+    }
+}
