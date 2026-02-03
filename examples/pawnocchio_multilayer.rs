9539933ffad0e5339fc5abd203939d36410509a3
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-02-03 20:33:53 +0100

    add ft0 norm loss penalty to encourage sparsity

--- examples/pawnocchio_multilayer.rs
+++ examples/pawnocchio_multilayer.rs
@@ -148,8 +148,7 @@ fn main() {
             SavedFormat::id("l3w").round().quantise::<i32>(Q as i32),
             SavedFormat::id("l3b").round().quantise::<i32>((Q as i32).pow(4)),
         ])
-        .loss_fn(|output, target| output.sigmoid().squared_error(target))
-        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
+        .build_custom(|builder, (stm_inputs, ntm_inputs, output_buckets), target| {
             // input layer factoriser
             let l0f = builder.new_weights("l0f", Shape::new(L1, 768), InitSettings::Zeroed);
             let expanded_factoriser = l0f.repeat(INPUT_BUCKETS);
@@ -166,15 +165,24 @@ fn main() {
             // inference
             let stm_hidden = l0.forward(stm_inputs).crelu().pairwise_mul();
             let ntm_hidden = l0.forward(ntm_inputs).crelu().pairwise_mul();
-            let hl1 = stm_hidden.concat(ntm_hidden);
+            let l0_out = stm_hidden.concat(ntm_hidden);
 
-            let l1_out = l1.forward(hl1).select(output_buckets);
+            let ones_l1_vec = builder.new_constant(Shape::new(1, L1), &[1.0 / L1 as f32; L1]);
+            let l0_out_norm = ones_l1_vec.matmul(l0_out);
+
+            let l1_out = l1.forward(l0_out).select(output_buckets);
             let hl2 = l1_out.concat(l1_out.abs_pow(2.0)).crelu();
 
             let l2_out = l2.forward(hl2).select(output_buckets);
             let hl3 = l2_out.crelu();
 
-            l3.forward(hl3).select(output_buckets)
+            let l3_out = l3.forward(hl3).select(output_buckets);
+
+            let loss = l3_out.sigmoid().squared_error(target);
+
+            let loss = loss + 0.005 * l0_out_norm;
+
+            (l3_out, loss)
         });
     let l0_clip = OptimiserParams { max_weight: 0.99, min_weight: -0.99, ..Default::default() };
     trainer.optimiser.set_params_for_weight("l0w", l0_clip);
